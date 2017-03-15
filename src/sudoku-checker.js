0ba7f4cc75fb6bda20de26ab40b73112ecfbe31f
Daniel da Silva <daniel@daniel.silva>
2017-03-15 08:31:06 -0300

    Sudoku checker: Check lines and columns

--- src/sudoku-checker.js
+++ src/sudoku-checker.js
@@ -1,9 +1,29 @@
 const isInvalidList = (list) => {
   return list.some((item) => !item ) ||
-         list.some((item, index) => list.slice(index + 1).some(curr => curr === item))
+         list.slice().sort()
+          .some((item, i, cList) => item === cList[i + 1]);
 }
 
+const invertBoard = (board) => {
+  return board.reduce((prev, curr) => {
+    curr.forEach((value, index) => {
+      prev[index] = prev[index] || [];
+      prev[index].push(value);
+    });
+    return prev;
+  }, []);
+}
 
-const doneOrNot = (board) => {
-  return 'Finished!';
+function doneOrNot(board){
+  let done = true;
+
+  // Check Lines
+  done = !board.some(isInvalidList);
+
+  // Check columns
+  if (done) {
+    done = done && !invertBoard(board).some(isInvalidList);
+  }
+
+  return done ? 'Finished!' : 'Try again!';
 }
