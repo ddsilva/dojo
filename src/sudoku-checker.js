7aac0fc7d156e31adbf6260e797906f8bce5eac5
Daniel Silva <daniel5055@gmail.com>
2017-03-15 01:10:24 -0300

    Sudoku checker: Done or not initial implementation

--- src/sudoku-checker.js
+++ src/sudoku-checker.js
@@ -2,3 +2,8 @@ const isInvalidList = (list) => {
   return list.some((item) => !item ) ||
          list.some((item, index) => list.slice(index + 1).some(curr => curr === item))
 }
+
+
+const doneOrNot = (board) => {
+  return 'Finished!';
+}
