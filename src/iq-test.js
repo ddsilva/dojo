b7eafc736c4693c2069be52d3b8750db239108b8
Daniel Silva <daniel5055@gmail.com>
2017-03-17 22:34:25 -0300

    IQ Test

---
+++ src/iq-test.js
@@ -0,0 +1,9 @@
+const iqTest = (numbers) => {
+  const list = numbers.split(' ').map(Number),
+        isOdd = ((n) => n % 2),
+        odds = list.filter(isOdd),
+        evens = list.filter((n) => !isOdd(n)),
+        value = (odds.length == 1 ? odds : evens)[0];
+
+  return list.indexOf(value) + 1;
+}
---
+++ test/iq-test-spec.js
@@ -0,0 +1,12 @@
+
+describe('IQ Test', () => {
+  it('should find the odd one position', () => {
+    expect(iqTest("2 4 7 8 10")).toBe(3);
+    expect(iqTest("2 4 6 7 8 10")).toBe(4);
+  });
+
+  it('should find the even one position', () => {
+    expect(iqTest("1 2 1 1")).toBe(2);
+    expect(iqTest("1 1 2 1 1")).toBe(3);
+  });
+});
