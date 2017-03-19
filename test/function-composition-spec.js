f29fce91574a6894864064efb2d2436310678921
Daniel Silva <daniel5055@gmail.com>
2017-03-19 11:31:57 -0300

    Function composition: Refactoring

--- test/function-composition-spec.js
+++ test/function-composition-spec.js
@@ -1,18 +1,23 @@
 describe('Function composition', () => {
+  const add1    = ((a) => a + 1),
+        min1    = ((a) => a - 1),
+        div2    = ((a) => a / 2),
+        add15   = ((a) => a + 15),
+        id      = ((a) => a),
+        addall5 = ((a, b, c, d, e) => a + b + c + d + e);
+
   it('should compose functions', () => {
-    expect(compose(compositionHelper.add1, compositionHelper.id)(3)).toBe(4);
-    expect(compose(compositionHelper.add15, compositionHelper.id)(0)).toBe(15);
-    expect(compose(compositionHelper.id, compositionHelper.add15)(0)).toBe(15);
-    expect(compose(compositionHelper.id, compositionHelper.min1)(0)).toBe(-1);
+    expect(compose(add1, id)(3)).toBe(4);
+    expect(compose(add15, id)(0)).toBe(15);
+    expect(compose(id, add15)(0)).toBe(15);
+    expect(compose(id, min1)(0)).toBe(-1);
   });
 
   it('order should be as specified', () => {
-    expect(compose(compositionHelper.add1, compositionHelper.div2)(2)).toBe(2);
+    expect(compose(add1, div2)(2)).toBe(2);
   });
 
   it('Functions may be called with many arguments.', () => {
-    expect(
-      compose(compositionHelper.add1, compositionHelper.addall5)(1,2,3,4,5)
-    ).toBe(16);
+    expect(compose(add1, addall5)(1,2,3,4,5)).toBe(16);
   });
 });
