25a04aa783a9eb9a79ed68c9b7c6c02da9eb9a03
ddsilva <daniel5055@gmail.com>
2017-03-20 09:45:48 -0300

    Valid Braces

---
+++ test/valid-braces-spec.js
@@ -0,0 +1,22 @@
+describe('Valid braces function', () => {
+  it('should return true for valid vraces', () => {
+    expect(validBraces('()')).toBe(true);
+    expect(validBraces('[]')).toBe(true);
+    expect(validBraces('{}')).toBe(true);
+    expect(validBraces('(){}[]')).toBe(true);
+    expect(validBraces('([{}])')).toBe(true);
+    expect(validBraces('({})[({})]')).toBe(true);
+    expect(validBraces('(({{[[]]}}))')).toBe(true);
+    expect(validBraces('{}({})[]')).toBe(true);
+  });
+
+  it('should return false for invalid braces', () => {
+    expect(validBraces('(}')).toBe(false);
+    expect(validBraces('[(])')).toBe(false);
+    expect(validBraces('(})')).toBe(false);
+    expect(validBraces(')(}{][')).toBe(false);
+    expect(validBraces('())({}}{()][][')).toBe(false);
+    expect(validBraces('(((({{')).toBe(false);
+    expect(validBraces('}}]]))}])')).toBe(false);
+  });
+});
